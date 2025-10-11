fc8667477ebd3bd1ae37992e0ca730aabe2ff6b6
TransposeDev <tofexzyno@gmail.com>
2025-10-11 22:06:26 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Layout from "./layout/Layout.tsx";
-import Dev from "./pages/Dev.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Home from "./pages/Home";
+import Layout from "./layout/Layout";
+import Dev from "./pages/Dev";
+import NotFound from "./pages/NotFound";
 import ThemeContext from "./context/ThemeContext";
-import Resume from "./pages/Resume.tsx";
+import Resume from "./pages/Resume";
 
 const App = () => {
   return (
