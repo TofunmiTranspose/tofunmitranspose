0a9214937aa8380fc15fc448b1565a765866677a
TransposeDev <tofexzyno@gmail.com>
2025-10-12 06:27:57 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../src/pages/Home";
+import Home from "./pages/Home";
 import Layout from "./layout/Layout";
 import Dev from "./pages/Dev";
 import NotFound from "./pages/NotFound";
 import ThemeContext from "./context/ThemeContext";
-//import Resume from "./pages/Resume";
+import Resume from "./pages/Resume";
 
 const App = () => {
   return (
     <BrowserRouter>
       <ThemeContext>
         <Routes>
+          {/* Routes with layout */}
           <Route element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="dev" element={<Dev />} />
             <Route path="*" element={<NotFound />} />
           </Route>
 
-          {/*  <Route path="resume" element={<Resume />} /> */}
+          {/* Standalone route (no layout) */}
+          <Route path="resume" element={<Resume />} />
         </Routes>
       </ThemeContext>
     </BrowserRouter>
